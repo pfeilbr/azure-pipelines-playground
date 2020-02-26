344a8c3e99f85dd5cca920050c3e6799b56d57f9
Brian Pfeil <brian.pfeil@gmail.com>
2020-02-26 12:26:12 -0500

    added profiling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -88,4 +88,10 @@ main() {
   fi
 }
 
+start_time="$(date -u +%s)"
 main "$@"
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "Total of $elapsed seconds elapsed for process"
+
+
