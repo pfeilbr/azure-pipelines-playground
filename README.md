4d227e6ec509fc77f7c92c03fde6a82bd15d8387
Brian Pfeil <brian.pfeil@gmail.com>
2020-02-26 16:40:52 -0500

    added cloudformation for provisioning

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ learn [azure pipelines](https://docs.microsoft.com/en-us/azure/devops/pipelines/
 ## Running
 
 ```sh
+# provision aws resources
+chmod +x scripts/provision.sh    
+./scripts/provision.sh
+
 # trigger a pipeline run via a tag
 TAG_NAME="v0.0.5"
 git tag -a "${TAG_NAME}" -m "releasing version ${TAG_NAME}"
---
+++ scripts/provision.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o noglob
+
+STACK_NAME="automation"
+aws cloudformation deploy \
+    --template cfn-templates/${STACK_NAME}-stack.yaml \
+    --stack-name ${STACK_NAME}-stack \
+    --parameter-overrides Password=automation123 \
+    --capabilities CAPABILITY_IAM
