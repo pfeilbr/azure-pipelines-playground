57c7ecafe55b7503e9a6a5b7d4327d94d32fd66e
Brian Pfeil <brian.pfeil@gmail.com>
2020-02-26 10:11:37 -0500

    fix colors

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ sync_s3() {
 
   echo -e "Syncing assets..."
   echo aws s3 sync ./public s3://${bucket_name}/${tag_name} --delete
-  echo -e "${green}Done${reset}"
+  echo -e "Done"
 }
 
 change_origin_path() {
@@ -30,7 +30,7 @@ change_origin_path() {
 #   next_distribution_config=$(bin/lib/update-distribution ${tag_name} "${current_distribution_config}")
 #   etag=$(bin/lib/get-etag "${current_distribution_config}")
   echo aws cloudfront update-distribution --id ${cloudfront_distribution_id} --distribution-config ${next_distribution_config} --if-match ${etag}
-  echo -e "${green}Done${reset}"
+  echo -e "Done"
 }
 
 invalidate_cache() {
@@ -38,7 +38,7 @@ invalidate_cache() {
 
   echo -e "Invalidating cloudfront cache..."
   echo aws cloudfront create-invalidation --distribution-id ${cloudfront_distribution_id} --paths /*
-  echo -e "${green}Done${reset}"
+  echo -e "Done"
 }
 
 deploy_to_git_tag() {
